41af0ae8dac97a1a5f5d094de781a9331cf9d24c
wzrabbit <87642422+wzrabbit@users.noreply.github.com>
2021-09-27 15:24:04 +0900

    생성: 기본 이미지 파일 업로드

---
+++ images/index.js
@@ -0,0 +1,11 @@
+const MainImage = {
+  logo: require("./icon_application.png").default,
+  title: require("./icon_ARK.png").default,
+  donkey: require("./icon_donkey.png").default,
+  minase: require("./icon_minaselogo.png").default,
+  bg: require("./img_background.png").default
+}
+
+export default MainImage;
+
+// 메인 이미지
