c930f89d5d0d1d9e7d8ed01e8017e9d2323b8489
nzkonaru <nzkonaru@gmail.com>
2021-09-28 14:38:28 +0900

    수정 : 레이아웃 오류 수정

--- src/components/main.js
+++ src/components/main.js
@@ -5,24 +5,26 @@ import Menu3 from './menu/menu3';
 import Menu4 from './menu/menu4';
 
 const Main = () => (
-  <div id="content-wrapper">
-    <div id="content-deco-1" />
-    <div id="content-deco-2" />
-    <div id="content">
-      <Switch id="content">
-        <Route exact path="/menu1">
-          <Menu1 />
-        </Route>
-        <Route exact path="/menu2">
-          <Menu2 />
-        </Route>
-        <Route exact path="/menu3">
-          <Menu3 />
-        </Route>
-        <Route exact path="/menu4">
-          <Menu4 />
-        </Route>
-      </Switch>
+  <div id="mid-wrapper">
+    <div id="content-wrapper">
+      <div id="content-deco-1" />
+      <div id="content-deco-2" />
+      <div id="content">
+        <Switch id="content">
+          <Route exact path="/menu1">
+            <Menu1 />
+          </Route>
+          <Route exact path="/menu2">
+            <Menu2 />
+          </Route>
+          <Route exact path="/menu3">
+            <Menu3 />
+          </Route>
+          <Route exact path="/menu4">
+            <Menu4 />
+          </Route>
+        </Switch>
+      </div>
     </div>
     <div id="layout-pusher" />
   </div>
