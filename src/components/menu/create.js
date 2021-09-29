1bf615a3bed68b08d40b9b5af48cdcbb00673ca8
wzrabbit <uicheonkim@gmail.com>
2021-09-29 16:49:09 +0900

    수정: 레이아웃 크기 일부 수정, 작전 기록 레이아웃 일부 추가(조금만 구현됨)
    UI 디자이너분 요청으로 새로 회의를 해야 할 것 같으니, 작업은 여기서 멈추겠습니다

--- src/components/menu/create.js
+++ src/components/menu/create.js
@@ -2,7 +2,7 @@ const Create = () => (
   <>
     <div className="content-deco-1" />
     <div className="content-deco-2" />
-    <div className="content">작전 생성 메뉴</div>
+    <div className="content create">작전 생성 메뉴</div>
   </>
 );
 
--- src/components/menu/record.js
+++ src/components/menu/record.js
@@ -2,9 +2,27 @@ const Record = () => (
   <>
     <div className="content-deco-1" />
     <div className="content-deco-2" />
-    <div className="content record1">작전 기록 부분 메뉴</div>
-    <div className="content record2">작전 선택 부분 메뉴</div>
+    <div className="content record-left">
+      <div id="record-title">
+        AUTHORIZED ONLY
+        <br />
+        RESTRICTED INFORMATION
+      </div>
+      <div id="record-author" />
+      <div id="record-date" />
+      <div className="content record-info" />
+      <div className="content record-restriction" />
+      <div className="content record-score" />
+    </div>
+    <div className="content record-right"></div>
   </>
 );
+
+/*
+ID 접근 방법 :
+기록 이름 -> record-title
+기록 작성자 -> record-author
+기록 날짜 -> record-date
+*/
 
 export default Record;
